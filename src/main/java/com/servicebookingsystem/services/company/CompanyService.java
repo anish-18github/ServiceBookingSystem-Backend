bc5233f1334ef679011b77d42b2ae79a51513b4f
anish-18github <anishgaikar17@gmail.com>
2024-08-19 18:57:50 +0530

    post ad api created

--- src/main/java/com/servicebookingsystem/services/company/CompanyService.java
+++ src/main/java/com/servicebookingsystem/services/company/CompanyService.java
@@ -1,5 +1,10 @@
 package com.servicebookingsystem.services.company;
 
+import java.io.IOException;
+
+import com.servicebookingsystem.dto.AdDTO;
+
 public interface CompanyService {
 
+	boolean postAd(Long userId, AdDTO adDTO) throws IOException;
 }
--- src/main/java/com/servicebookingsystem/services/company/CompanyServiceImpl.java
+++ src/main/java/com/servicebookingsystem/services/company/CompanyServiceImpl.java
@@ -1,10 +1,20 @@
 package com.servicebookingsystem.services.company;
 
+
+
+import java.io.IOException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.servicebookingsystem.dto.AdDTO;
+import com.servicebookingsystem.entity.Ad;
+import com.servicebookingsystem.entity.User;
 import com.servicebookingsystem.repository.AdRepository;
 import com.servicebookingsystem.repository.UserRepository;
+
+
 
 @Service
 public class CompanyServiceImpl implements CompanyService{
@@ -14,4 +24,23 @@ public class CompanyServiceImpl implements CompanyService{
 	
 	@Autowired
 	private AdRepository adRepository;
+	
+	public boolean postAd(Long userId, AdDTO adDTO) throws IOException{
+		Optional<User> optionalUser = userRepository.findById(userId);
+		if (optionalUser.isPresent()) {
+			Ad ad = new Ad();
+			ad.setServiceName(adDTO.getServiceName());
+			ad.setDescription(adDTO.getDescription());
+			ad.setImg(adDTO.getImg().getBytes());
+			ad.setPrice(adDTO.getPrice());
+			ad.setUser(optionalUser.get());
+			
+			
+			adRepository.save(ad);
+			return true;
+			
+		}
+		return false;
+		
+	}
 }
