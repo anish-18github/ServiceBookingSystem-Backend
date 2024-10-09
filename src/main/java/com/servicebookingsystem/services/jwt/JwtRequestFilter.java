c3fb6038a5b58302ad6a0f5d658407661230ca00
anish-18github <anishgaikar17@gmail.com>
2024-10-09 20:38:59 +0530

    Non Null Annotation added

--- src/main/java/com/servicebookingsystem/services/jwt/JwtRequestFilter.java
+++ src/main/java/com/servicebookingsystem/services/jwt/JwtRequestFilter.java
@@ -3,6 +3,7 @@ package com.servicebookingsystem.services.jwt;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,7 +28,7 @@ public class JwtRequestFilter extends OncePerRequestFilter{
 	private JwtUtil jwtUtil;
 	
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
+	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException{
 		String authHeader = request.getHeader("Authorization");
 		String token = null;
 		String username = null;
